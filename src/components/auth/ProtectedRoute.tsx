c7e13b3a2a5bad7ee7a17c83d43c2bfd0e20aa37
jungho-Kang <rkdwjdgh08@gmail.com>
2026-03-05 16:59:02 +0900

    [fix] vercel 오류 수정

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
-import { useSessionAuth } from "@/hooks/useSessionAuth";
-import { Navigate } from "react-router-dom";
+// import { useSessionAuth } from "@/hooks/useSessionAuth";
+// import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isLogin } = useSessionAuth();
+  // const { isLogin } = useSessionAuth();
 
   // if (!isLogin) {
   //   return <Navigate to="/auth" replace />;
