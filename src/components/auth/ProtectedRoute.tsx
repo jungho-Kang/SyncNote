119ddb06cfea9f8ecdbc63c624e6153f14ad6f29
jungho-Kang <rkdwjdgh08@gmail.com>
2026-03-17 20:27:09 +0900

    [feat] 로그인 회원가입 기능 복구

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,12 +1,12 @@
-// import { useSessionAuth } from "@/hooks/useSessionAuth";
-// import { Navigate } from "react-router-dom";
+import { useSessionAuth } from "@/hooks/useSessionAuth";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  // const { isLogin } = useSessionAuth();
+  const { isLogin } = useSessionAuth();
 
-  // if (!isLogin) {
-  //   return <Navigate to="/auth" replace />;
-  // }
+  if (!isLogin) {
+    return <Navigate to="/auth" replace />;
+  }
 
   return <>{children}</>;
 };
